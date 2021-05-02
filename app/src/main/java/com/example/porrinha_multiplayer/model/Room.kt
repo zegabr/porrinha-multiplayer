d13ca6b4b5943b907ce74b108c56a0a50ca65fc1
rodrigoap2 <rap2@cin.ufpe.br>
2021-05-02 00:20:43 -0300

    Create room screen

--- app/src/main/java/com/example/porrinha_multiplayer/model/Room.kt
+++ app/src/main/java/com/example/porrinha_multiplayer/model/Room.kt
@@ -4,8 +4,9 @@ data class Room(
         var currentRound: Int? = 0,
         var lastRoundSticks: Int? = -1,
         var maxRounds: Int? = 0,
-        var players: Map<String,Player>? = null,
+        var players: Map<String,Player>? = emptyMap<String,Player>(),
         var latitude: Double = 0.0,
         var longitude: Double = 0.0,
-        var name: String = ""
+        var name: String = "",
+        var maxPlayers: Int? = 0
 )
